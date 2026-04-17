ad786d0ee3fccdcff32898b243b8feb9656d4f3e
Agung rahma <agungrahmasuputraa@gmail.com>
2026-04-17 12:08:10 +0800

    Adding page controller for specify job in control and routing at all page in NSS portfolio

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index() {
+        return view('index');
+    }
+    public function team() {
+        return view('team');
+    }
+    public function gallery() {
+        return view('gallery'); 
+    }
+}
